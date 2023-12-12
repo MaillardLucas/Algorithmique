85834835ba3b6f9b184a18763e55824e5c40c4b5
Maillard <151616522+MaillardLucas@users.noreply.github.com>
2023-12-12 11:53:37 +0100

    modif

---
+++ exo2.py
@@ -0,0 +1,17 @@
+def compter_lettres(mot):
+    return len(mot)
+
+mots_saisis = []
+
+while True:
+    mot = input("Saisissez un mot (ou tapez 'STOP' pour arrêter) : ")
+    
+    if mot.upper() == "STOP":
+        break
+    
+    mots_saisis.append(mot)
+
+print("\nNombre de lettres dans chaque mot saisi :")
+for mot in mots_saisis:
+    nombre_de_lettres = compter_lettres(mot)
+    print(f"{mot}: {nombre_de_lettres} lettres")
